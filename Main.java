bbd9625db7ddc339e3d6140b502bb39d283c7dd8
khametamaksim <61475616+khametamaksim@users.noreply.github.com>
2020-05-29 02:44:15 +0200

    Klasa main

---
+++ Main.java
@@ -0,0 +1,16 @@
+//import
+
+public class Main {
+	
+	public static void main (String[] args) {
+		int x = 100;
+		int y 100;
+		int[][] cells;
+	}
+
+	public int generation (cells[][], n) {
+	}
+
+	public void savetofile (x, y, cells[][]) {
+	}
+}
